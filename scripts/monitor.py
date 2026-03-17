11eb11d227537d64d576e8bd60c8d5ceb356cc0d
aishaa08 <aisha.a.kurbanova@gmail.com>
2026-03-17 11:16:40 +0100

    oppdatert monitor script

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -1,6 +1,24 @@
 import psutil
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="PASSORD123",
+    database="server_monitor"
+)
+
+cursor = conn.cursor()
+
 cpu = psutil.cpu_percent()
 ram = psutil.virtual_memory().percent
 
-print("CPU bruk:", cpu)
-print("RAM bruk:", ram)
+sql = "INSERT INTO system_status (cpu, ram) VALUES (%s, %s)"
+cursor.execute(sql, (cpu, ram))
+
+conn.commit()
+conn.close()
+
+print("Data lagret i databasen")
+print("CPU:", cpu)
+print("RAM:",ram)
