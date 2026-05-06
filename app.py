10a99e37eb55614dbbbfa389f3a81f4e0c58bbd3
aishaa08 <aisha.a.kurbanova@gmail.com>
2026-05-06 12:19:35 +0200

    endringer

--- app.py
+++ app.py
@@ -22,5 +22,20 @@ def index():
     data = get_data()
     return render_template("index.html", data=data)
 
+@app.route("/ansatte")
+def ansatte():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="PASSORD123",
+        database="server_monitor"
+        
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM ansatte")
+    data = cursor.fetchall()
+    conn.close()
+    return render_template("ansatte.html", data=data)
+
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
